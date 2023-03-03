077f2eb1bebdcc8d6fc119f80ecba6970505b05d
Azizbek Habibov <azizbek.habibov@techcells.com>
2023-03-03 10:07:45 +0500

    Telegram service

---
+++ src/main/java/uz/md/shopapp/dtos/bot/OrderProductAddToBotDTO.java
@@ -0,0 +1,21 @@
+package uz.md.shopapp.dtos.bot;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OrderProductAddToBotDTO {
+    @NotNull(message = "ordered product name can not be null ")
+    private String name;
+    @NotNull(message = "ordered product count can not be null ")
+    private int count;
+
+    @NotNull(message = "ordered product price can not be null ")
+    private double price;
+    @NotNull(message = "ordered product institution name can not be null ")
+    private String institutionName;
+}
---
+++ src/main/java/uz/md/shopapp/dtos/bot/OrderSendToBotDto.java
@@ -0,0 +1,29 @@
+package uz.md.shopapp.dtos.bot;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import uz.md.shopapp.dtos.institution.LocationDto;
+import uz.md.shopapp.dtos.order.OrderProductAddDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OrderSendToBotDto {
+
+    @NotNull(message = "phone number must not be null")
+    private String clientPhoneNumber;
+    @NotNull(message = "location must not be null")
+    private LocationDto location;
+    private LocalDateTime deliveryTime = LocalDateTime.now();
+    @NotNull(message = "ordered products must not be null")
+    private List<OrderProductAddToBotDTO> orderProducts;
+
+    @NotNull(message = "total price must not be null")
+    private double totalPrice;
+}
