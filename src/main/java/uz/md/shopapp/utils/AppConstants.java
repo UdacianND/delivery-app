47534bfeed79315715281c9b2f9aed8b92f2011a
Azizbek Habibov <azizbek.habibov@techcells.com>
2023-03-03 10:07:45 +0500

    Telegram service

--- src/main/java/uz/md/shopapp/utils/AppConstants.java
+++ src/main/java/uz/md/shopapp/utils/AppConstants.java
@@ -23,14 +23,15 @@ public interface AppConstants {
             InstitutionController.BASE_URL+"/by-page/*",
             InstitutionController.BASE_URL+"/*",
             InstitutionController.BASE_URL+"/by-type/*",
-            InstitutionController.BASE_URL+"/all/by-type/*",
+            InstitutionController.BASE_URL+"/all/by_type/*",
             ProductController.BASE_URL+"/category/*",
             CategoryController.BASE_URL,
             CategoryController.BASE_URL+"/all",
             CategoryController.BASE_URL+"/*",
             CategoryController.BASE_URL+"/institution/*",
             CategoryController.BASE_URL+"/institution/*/by-page/*",
-            AuthController.BASE_URL + "/**"
+            AuthController.BASE_URL + "/**",
+            TelegrambotController.BASE_URL + "/**"
     };
 
     String[] SWAGGER_PAGES = {
@@ -63,5 +64,18 @@ public interface AppConstants {
      */
     String PhoneNumber_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+
+    /**
+    *Telegram bot constants
+     */
+    String TELEGRAM_BASE_URL = "https://api.telegram.org/bot";
+    String TELEGRAM_BOT_WEBAPP_URL = "https://master--melodic-meerkat-622f75.netlify.app/";
+    String BOT_TOKEN = "6197877428:AAF1jOddZ97jKfyQ-dFqAi_tMV5o8yLtMbk";
+    String TELEGRAM_BOT_FULL_URL = TELEGRAM_BASE_URL.concat(BOT_TOKEN);
+    String TELEGRAM_SEND_MESSAGE_URL = TELEGRAM_BOT_FULL_URL + "/sendMessage";
+    String GROUP_CHAT_ID = "@delivery_channel";
+    String TELEGRAM_SEND_LOCATION_URL = TELEGRAM_BOT_FULL_URL + "/sendLocation?chat_id="+GROUP_CHAT_ID ;
+
+    String BOT_STARTER_MESSAGE = "Salom, ushbu havolani oching";
 
 }
